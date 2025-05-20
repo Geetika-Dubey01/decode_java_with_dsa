02da21e64a495d373e1907370dcbd4ac80c7bc1f
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-20 09:20:03 +0530

    Sorting

---
+++ PlusOperator.java
@@ -0,0 +1,11 @@
+package strings;
+public class PlusOperator {
+    public static void main(String[] args) {
+        String s = "abc";
+        String t = "pqr";
+        s = s + t;
+        System.out.println(s);
+        System.out.println("hi i am" +10);
+        System.out.println("abc"+10+20);
+    }
+}
